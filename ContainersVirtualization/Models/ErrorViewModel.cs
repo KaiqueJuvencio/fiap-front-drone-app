db3b6ebae7236057ac054cfc4e90e19e80255813
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2023-01-11 22:56:21 -0300

    Initial Commit

---
+++ ContainersVirtualization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContainersVirtualization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
